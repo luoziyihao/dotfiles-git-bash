9b82e5e219c79f2a06825fcd74a58ba45fbfda81
luoziyihao <wangyiraoxiang@163.com>
2019-11-10 12:30:28 +0800

    tmp push

--- .alias.local.sh
+++ .alias.local.sh
@@ -9,6 +9,8 @@ alias cddl="cd ~/Downloads/"
 alias cdt="mkdir -p ~/WorkTemp/ && cd ~/WorkTemp/"
 alias cds="cd ~/Works/MY_PROJECT/shellManage"
 alias cdblog="cd ~/Works/MY_PROJECT/blog/source/_posts/"
+alias cdco="cd ~/Works/corp"
+alias cdsy="cd ~/Works/sy"
 
 
 alias sshone="ssh root@123.56.6.229"
@@ -16,6 +18,7 @@ alias sshvpn="ssh root@vpn.luoziyihao.com"
 
 
 alias hkpro="ssh -p 60022 raoxiang@jumpbox.huakai.me"
+alias cbpro="ssh -p 60022 raoxiang@39.108.102.65"
 alias hkdoc="cd ~/Works/hk/hk-docs/"
 alias hkapi="cd ~/Works/hk/hk-api/"
 alias hkagt="cd ~/Works/hk/hk-agent/"
